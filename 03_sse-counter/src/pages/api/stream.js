78843175e8f2b97a2ee264817fe521aa72f578cd
wass <wassim.filali@gmail.com>
2022-12-12 19:41:29 +0100

    SSE Counter example

---
+++ 03_sse-counter/src/pages/api/stream.js
@@ -0,0 +1,30 @@
+import {Emitter} from '../events'
+
+export async function get({request}){
+
+    console.log("stream.js> get()")
+    
+    const body = new ReadableStream({
+        start(controller){
+            const events_listener = (counter)=>{
+                console.log(`stream.js> Emitter 'count' = ${counter}`)
+                const data = `data: ${JSON.stringify({counter})}\r\n\r\n`;
+                controller.enqueue(data)
+            }
+            Emitter.off('count',events_listener)
+            Emitter.on('count',events_listener)
+        },
+        cancel(){
+            console.log("stream.js> cancel()")
+        }
+    })
+
+    return new Response(body, {
+        status: 200,
+        headers: {
+            'Content-Type': 'text/event-stream',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'no-cache'          
+        }
+    });
+}
