bf5d8a6ce4e2daacb13ff91ecc8b7369f2612cfd
wass <wassim.filali@gmail.com>
2022-12-17 15:12:24 +0100

    added github action

--- 07_env-config-deno/src/pages/server.js
+++ 07_env-config-deno/src/pages/server.js
@@ -1,4 +1,5 @@
 console.log(`process running in '${Deno.cwd()}'`)
+console.log(`impor t.meta.env.MY_VAR = ${import.meta.env.MY_VAR}`)
 console.log(`Deno.env.get("MY_VAR") = ${Deno.env.get("MY_VAR")}`)
 
 function get_var(){
