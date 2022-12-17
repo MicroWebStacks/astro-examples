c7f8d08e5e4ac23181bc2aff45d460fe9a7df21a
wass <wassim.filali@gmail.com>
2022-12-17 11:59:03 +0100

    added import

--- 07_env-config/src/pages/server.js
+++ 07_env-config/src/pages/server.js
@@ -4,6 +4,9 @@ console.log(`process running in '${process.cwd()}'`)
 dotenv.config({path:"./special.env"})
 
 console.log(`process.env.MY_VAR = ${process.env.MY_VAR}`)
+console.log(`impor t.meta.env.MY_VAR = ${import.meta.env.MY_VAR}`)
+
+
 
 function get_var(){
     return process.env.MY_VAR
