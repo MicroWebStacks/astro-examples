a4eef2f69c9f5e78f39d26b3e8b104d66e578261
wass <wassim.filali@gmail.com>
2023-10-14 10:01:41 +0200

    simplified remote markdown

--- 23_remote-markdown/astro.config.mjs
+++ 23_remote-markdown/astro.config.mjs
@@ -1,9 +1,4 @@
-import node from '@astrojs/node'
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: 'standalone'
-  })
 });
