c60391c7952ff12b3faa08066f392d6e1e418187
wass <wassim.filali@gmail.com>
2023-12-02 13:31:34 +0100

    added base

--- 00_website/astro.config.mjs
+++ 00_website/astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: "../docs"
+    outDir: "../docs",
+    base:"astro-examples"
 });
